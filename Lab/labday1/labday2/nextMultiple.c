916a0dc5658f751b2c378e2f9eb903dd2f014532
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2024-05-23 21:19:50 +1200

    solved lab2

--- Lab/labday1/labday2/nextMultiple.c
+++ Lab/labday1/labday2/nextMultiple.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main() {
- int m = 23;
- int n = (m/10+1)*10;    //3*10=30
- int o = (m/10)*10;      //2*10 = 20
-   
-   int mod = m%10;
- printf("%d",(mod>=5)? n :o);
+ int m = 42;
+ int r;
+//-----------method 1----------------
+//  int n = (m/10+1)*10;    //3*10=30
+//  int o = (m/10)*10;      //2*10 = 20
+//  int mod = m%10;
+//  printf("%d", (mod>=5) ? n : o);
+
+
+//-----------method 2----------------
+r = (m/10+m%10/5)*10;//2+5
+printf("%d", r);
+
 return 0;    
 }
